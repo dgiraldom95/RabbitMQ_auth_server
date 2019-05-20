16d7c2ca11af7fee72f242741c1da7a6e495a3de
dgiraldom95 <d.giraldo12@uniandes.edu.co>
2019-05-20 12:12:04 -0500

    arreglo ready

--- auth_api/apps.py
+++ auth_api/apps.py
@@ -7,17 +7,15 @@ class MyAppConfig(AppConfig):
     name = 'auth_api'
 
     def ready(self):
-        if "manage.py" not in sys.argv:
+        import threading
 
-            import threading
+        import auth_api.models
+        from auth_api import broker
 
-            import auth_api.models
-            from auth_api import broker
+        # Provee el servicio de generar JWT
+        autenticacion_JWT = broker.MessageBrokerServer(queue_name='obtener_jwt', callback=broker.autenticar_usuario)
+        threading.Thread(target=autenticacion_JWT.listen).start()
 
-            # Provee el servicio de generar JWT
-            autenticacion_JWT = broker.MessageBrokerServer(queue_name='obtener_jwt', callback=broker.autenticar_usuario)
-            threading.Thread(target=autenticacion_JWT.listen).start()
-
-            # Provee el servicio de autenticar a los usuarios con JWT
-            validar_JWT = broker.MessageBrokerServer(queue_name='validar_jwt', callback=broker.validar_jwt)
-            threading.Thread(target=validar_JWT.listen).start()
+        # Provee el servicio de autenticar a los usuarios con JWT
+        validar_JWT = broker.MessageBrokerServer(queue_name='validar_jwt', callback=broker.validar_jwt)
+        threading.Thread(target=validar_JWT.listen).start()
