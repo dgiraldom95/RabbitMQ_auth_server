e2ac306bd1e4f93954968d5724bb37e9dff42e70
dgiraldom95 <d.giraldo12@uniandes.edu.co>
2019-05-21 10:00:54 -0500

    JWT cookie

--- auth_server/settings.py
+++ auth_server/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = '1a-nh^$jm0^euvck0d(5nz324+!_l#3v7mz!j$9t%&xi(bp*y-'
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-
 
 # Application definition
 
@@ -72,7 +70,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'auth_server.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
@@ -105,7 +102,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
@@ -118,7 +114,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
@@ -147,5 +142,6 @@ REST_FRAMEWORK = {
     ),
 }
 
-
-
+JWT_AUTH = {
+    'JWT_AUTH_COOKIE': 'JWT',
+}
